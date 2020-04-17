caed92a7ac140f2251c6f2523e794ee5165299b2
Thomas John Helin <thomashelin@yahoo.com>
2020-04-17 17:21:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,7 @@ def all_holidays_with_bbq(holiday_hash)
    holiday.map do |holiday_name, supply_array|
      if supply_array.include? "BBQ"
        #binding.pry
-      holiday_name
+      holiday_name.compact
 end
 end
 end.flatten
