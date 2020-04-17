bc08bef821bcd600c418fe74052926e425c693ea
Thomas John Helin <thomashelin@yahoo.com>
2020-04-17 16:58:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,7 @@ def all_holidays_with_bbq(holiday_hash)
  holiday_hash.each do |season, holiday|
    holiday.each do |holiday_name, supply|
      if supply.include? "BBQ"
-       return holiday
+       return holiday.flatten
 end
 end
 end
